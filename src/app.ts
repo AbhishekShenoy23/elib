3afd90faaa3cce1019555151abcebca97cb548f4
AbhishekShenoy23 <abhishekshenoy90@gmail.com>
2024-04-30 18:50:00 +0530

    # user router and Controller

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { NextFunction, Request, Response } from "express";
 import { globalErrorHandler } from "./middleware/globalErrorHandler";
 import createHttpError from "http-errors";
+import userRouter from "./user/userRouter";
 
 const app = express();
 
@@ -10,6 +11,8 @@ app.get("/", (req: Request, res: Response, next: NextFunction) => {
   console.log("GET Request from Home");
   res.status(200).json({ message: "Hello from Home" });
 });
+
+app.use("/api/users", userRouter);
 
 //Global Error Handler- this will be written at the end.
 app.use(globalErrorHandler);
