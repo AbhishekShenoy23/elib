b16c42db9d208b79105a24299df53d61dac2eb18
AbhishekShenoy23 <abhishekshenoy90@gmail.com>
2024-04-30 11:46:12 +0530

    #Config Set up

--- server.ts
+++ server.ts
@@ -1,1 +1,13 @@
+import app from "./src/app";
 
+import { config } from "./src/config/config";
+
+const startServer = () => {
+  const port = config.port || 3005;
+
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+};
+
+startServer();
