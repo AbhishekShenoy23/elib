b1f69697de2442e94d13e572b1844493141f4380
AbhishekShenoy23 <abhishekshenoy90@gmail.com>
2024-04-30 20:24:01 +0530

    #6 add user register controller validation

--- src/app.ts
+++ src/app.ts
@@ -4,14 +4,16 @@ import createHttpError from "http-errors";
 import userRouter from "./user/userRouter";
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  const error = createHttpError(500, "Invalid");
-  throw error;
+  //const error = createHttpError(500, "Invalid");
+  //throw error;
   console.log("GET Request from Home");
   res.status(200).json({ message: "Hello from Home" });
 });
 
+//User Routes
 app.use("/api/users", userRouter);
 
 //Global Error Handler- this will be written at the end.
--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,6 +1,19 @@
 import { NextFunction, Request, Response } from "express";
+import createHttpError from "http-errors";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { name, email, password } = req.body;
+
+  //Validation
+  if (!name || !email || !password) {
+    const error = createHttpError(400, {
+      message: "All Fields are required",
+    });
+    return next(error);
+  }
+
+  //Process the data
+
   res.status(200).json({ message: "Hello from register controller" });
 };
 
