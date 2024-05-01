2a4c11a21b88c0c10be6e7a35a3135bbd6124634
AbhishekShenoy23 <abhishekshenoy90@gmail.com>
2024-05-01 09:38:56 +0530

    #7 Error handling in user register controller

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,6 +6,7 @@ const _config = {
   port: process.env.PORT,
   mongoDB: process.env.MONGODB_CONNECTION_URL,
   env: process.env.NODE_ENV,
+  secretKey: process.env.SECRET_KEY,
 };
 
 export const config = Object.freeze(_config); //this will  make _config read Only
--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,6 +1,9 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { config } from "../config/config";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
@@ -14,15 +17,48 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
   }
 
   //Process the data .. Database Call
-
-  const user = await userModel.findOne({ email });
-
-  if (user) {
-    const error = createHttpError(400, { message: "User Already Exists" });
-    return next(error);
+  let user;
+  try {
+    user = await userModel.findOne({ email });
+    if (user) {
+      const error = createHttpError(400, { message: "User Already Exists" });
+      return next(error);
+    }
+  } catch (error) {
+    return next(createHttpError(500, { message: "Error while fetching user" }));
   }
 
-  res.status(200).json({ message: "Hello from register controller" });
+  //Hashing the password .. This will be CPU intensive. should make sure await
+  //while hashing there might be a chance that password hashed will be of same type of pattern which will help the hacker to guess password.
+  //So we will use salt to make it more secure.
+
+  let newUser;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    newUser = await userModel.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+  } catch (error) {
+    return next(
+      createHttpError(500, { message: "Error while registering User" })
+    );
+  }
+
+  try {
+    //Token generation.
+
+    const token = jwt.sign({ sub: newUser._id }, config.secretKey as string, {
+      expiresIn: "1d",
+    });
+
+    res.status(200).json({ accessToken: token });
+  } catch (error) {
+    return next(
+      createHttpError(500, "Error while signing User. Generating token")
+    );
+  }
 };
 
 export { createUser };
