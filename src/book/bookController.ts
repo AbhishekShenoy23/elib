ed881ecbb970cd25970920ee60832ab8f4b6c3f8
AbhishekShenoy23 <abhishekshenoy90@gmail.com>
2024-05-01 15:44:31 +0530

    #10 Cloudinary initial setup

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 
 const createBooks = async (req: Request, res: Response, next: NextFunction) => {
-  console.log(req.body);
+  console.log(req.files);
   res.status(201).json({
     message: "Book Created",
   });
--- src/config/config.ts
+++ src/config/config.ts
@@ -7,6 +7,9 @@ const _config = {
   mongoDB: process.env.MONGODB_CONNECTION_URL,
   env: process.env.NODE_ENV,
   secretKey: process.env.SECRET_KEY,
+  cloudinary_cloud: process.env.CLOUDINARY_CLOUD,
+  cloudinary_api_key: process.env.CLOUDINARY_API_KEY,
+  cloudinary_api_secret: process.env.CLOUDINARY_API_SECRET,
 };
 
 export const config = Object.freeze(_config); //this will  make _config read Only
