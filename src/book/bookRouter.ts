f7bb229b2f03769aa73e773efbf4c65c72c3cb3c
AbhishekShenoy23 <abhishekshenoy90@gmail.com>
2024-05-01 15:20:47 +0530

    #9 create multer middleware for multi form data

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,8 +1,24 @@
 import express from "express";
+import path from "node:path";
 import { createBooks } from "./bookController";
+import multer from "multer";
 
 const bookRouter = express.Router();
 
-bookRouter.post("/createBooks", createBooks);
+//multer will first store file in server file system and then we will move file to cloud.
+
+const upload = multer({
+  dest: path.resolve(__dirname, "../../public/data/uploads"),
+  limits: { fileSize: 3e7 },
+});
+
+bookRouter.post(
+  "/",
+  upload.fields([
+    { name: "coverImage", maxCount: 1 },
+    { name: "file", maxCount: 1 },
+  ]),
+  createBooks
+);
 
 export default bookRouter;
